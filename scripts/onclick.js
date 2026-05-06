cb320fd912cae4002d5c9c72fb608cc5b578f17a
samuelmaciel015 <samuel.amorimmaciel015@gmail.com>
2026-05-06 17:02:34 -0300

    página inicio e curso completas

---
+++ scripts/onclick.js
@@ -0,0 +1,15 @@
+function open_facebook() {
+    window.open('https://www.facebook.com/panteraviation/?locale=pt_BR', '_blank');
+}
+function open_instagram() {
+    window.open('https://www.instagram.com/panteraviation/', '_blank');
+}
+function open_whatsapp() {
+    window.open('https://wa.me/551124470709?text=Olá! Gostaria de obter mais informações.', '_blank');
+}
+function go_top() {
+    window.scrollTo(0, 0);
+}
+function reload() {
+    window.location.reload();
+}
