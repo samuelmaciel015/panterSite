e9ee6f7062514e6bafadcb6ff249ac6245529b5a
Samuel Maciel <samuel.amorimmaciel015@gmail.com>
2026-09-10 16:16:44 -0300

    changed again

--- public/scripts/flipCards.js
+++ public/scripts/flipCards.js
@@ -1,8 +1,6 @@
 const carta1 = document.querySelector('.card1');
 const carta2 = document.querySelector('.card2');
 const carta3 = document.querySelector('.card3');
-const carta4 = document.querySelector('.card4');
-const carta5 = document.querySelector('.card5');
 
 carta1.addEventListener('click', () => {
     carta1.classList.toggle('flipped');
@@ -12,10 +10,4 @@ carta2.addEventListener('click', () => {
 });
 carta3.addEventListener('click', () => {
     carta3.classList.toggle('flipped');
-});
-carta4.addEventListener('click', () => {
-    carta4.classList.toggle('flipped');
-});
-carta5.addEventListener('click', () => {
-    carta5.classList.toggle('flipped');
 });
